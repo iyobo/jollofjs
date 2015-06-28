fbd00b907d49ab8c0ca1f4620a75d8f195d04ce4
Kristian Trenskow <trenskow@me.com>
2015-06-28 22:30:20 +0200

    Use `typeof` instead of `constructor.name`. Closed #4.

--- packages/isvalid/lib/ranges.js
+++ packages/isvalid/lib/ranges.js
@@ -1,54 +1,54 @@
 module.exports.testIndex = function(ranges, index) {
-	
+
 	// Convert to string if ranges is a Number.
-	if (ranges !== undefined && 'Number' == ranges.constructor.name) {
+	if (ranges !== undefined && typeof ranges === 'number') {
 		ranges = ranges.toString();
 	}
-	
+
 	// Throw if ranges is not a string.
-	if (!ranges || 'String' != ranges.constructor.name) {
+	if (!ranges || typeof ranges !== 'string') {
 		throw new Error('Ranges must be a number or a string expressed as: ex. \'-2,4-6,8,10-\'.');
 	}
-	
+
 	// Throw if index is not a number.
-	if (index === undefined || 'Number' != index.constructor.name) {
+	if (index === undefined || typeof index !== 'number') {
 		throw new Error('Index is not a number.');
 	}
-	
+
 	// Split into individual ranges.
 	var r = ranges.split(',');
 	for (var idx in r) {
-		
+
 		// Get the boundaries of the range.
 		var boundaries = r[idx].split('-');
-		
+
 		// Low and high boundaries are the same where only one number is specified.
 		if (boundaries.length == 1) boundaries = [ boundaries[0], boundaries[0] ];
 		// Throw an error if there is not exactly to boundaries.
 		if (boundaries.length != 2) throw new Error('Malformed range \'' + r[idx] + '\'.');
-		
+
 		// Test for malformed boundaries
 		for (var bidx = 0 ; bidx < 2 ; bidx++) {
 			if (!/^[0-9]*$/.test(boundaries[bidx])) throw new Error('Malformed boundary \'' + boundaries[bidx] + '\'.');
 		};
-		
+
 		var lowBoundary = boundaries[0];
 		var highBoundary = boundaries[1];
-		
+
 		// If no lower boundary is specified we use -Infinity
 		if (lowBoundary.length == 0) lowBoundary = -Infinity;
 		else lowBoundary = parseInt(lowBoundary);
-		
+
 		// If no higher boundary is specified we use Infinity;
 		if (highBoundary.length == 0) highBoundary = Infinity;
 		else highBoundary = parseInt(highBoundary);
-		
+
 		// If index is within boundaries return true;
 		if (index >= lowBoundary && index <= highBoundary) return true;
-		
+
 	}
-	
+
 	// Index was not matched to any range.
 	return false;
-	
+
 };
--- packages/isvalid/lib/unique.js
+++ packages/isvalid/lib/unique.js
@@ -1,11 +1,11 @@
 var objectEquals = function(obj1, obj2, fn) {
-	
+
 	var keys = Object.keys(obj1);
-	
+
 	arrayEquals(Object.keys(obj1), Object.keys(obj2), function(res) {
-		
+
 		if (!res) return fn(false);
-		
+
 		(function testNext(idx) {
 			if (idx == keys.length) return fn(true);
 			var key = keys[idx];
@@ -16,18 +16,18 @@ var objectEquals = function(obj1, obj2, fn) {
 				});
 			});
 		})(0);
-		
+
 	});
-	
+
 };
 
 var arrayEquals = function(obj1, obj2, fn) {
-	
+
 	if (obj1.length != obj2.length) return fn(false);
-	
+
 	var o1 = obj1.sort();
 	var o2 = obj2.sort();
-	
+
 	(function testNext(idx) {
 		if (idx == o1.length) return fn(true);
 		equals(o1[idx], o2[idx], function(res) {
@@ -37,25 +37,25 @@ var arrayEquals = function(obj1, obj2, fn) {
 			});
 		});
 	})(0);
-	
+
 };
 
 var equals = function(obj1, obj2, fn) {
-	
+
 	if ((obj1 && !obj2) || (!obj1 && obj2)) return fn(false);
-	if (obj1.constructor.name != obj2.constructor.name) return fn(false);
-	
-	if ('Object' == obj1.constructor.name) return objectEquals(obj1, obj2, fn);
-	if ('Array' == obj1.constructor.name) return arrayEquals(obj1, obj2, fn);
-	
+	if (typeof obj1 !== typeof obj2) return fn(false);
+
+	if (typeof obj1 === 'object') return objectEquals(obj1, obj2, fn);
+	if (typeof obj1 === 'array') return arrayEquals(obj1, obj2, fn);
+
 	return fn(obj1 == obj2);
-	
+
 };
 
 module.exports = function(arr, fn) {
-	
+
 	if (arr.length <= 1) return fn(true);
-	
+
 	(function testNext(idx1, idx2) {
 		if (idx2 == arr.length) {
 			idx1++;
@@ -69,7 +69,7 @@ module.exports = function(arr, fn) {
 			});
 		});
 	})(0, 1);
-	
+
 }
 
 // We export equals so it can be unit tested.
