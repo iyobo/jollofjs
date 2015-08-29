f83bbc8bc841342fa99838bb4094535bfe4d2ce8
Kristian Trenskow <trenskow@me.com>
2015-08-29 23:29:12 +0200

    Made middleware use sync finalizer.

--- packages/isvalid/lib/middleware.js
+++ packages/isvalid/lib/middleware.js
@@ -1,67 +1,41 @@
+var formalize = require('./schema.js').formalize;
 var isvalid = require('./validate.js');
 
-var middleware = function(key, schema, options) {
+exports.body = module.exports.body = function(schema, options) {
 
-	// We don't want the finalizer to work on every request.
-	// So we finalize on the first request and then reuse
-	// the finalized schema for future requests.
-
-	// The final finalized schame.
-	var finalSchema;
+	var formalizedSchema = formalize(schema);
 
 	return function(req, res, next) {
-
-		// The actual validation function
-		function validate() {
-			isvalid(req[key], finalSchema, function(err, validObj) {
-				req[key] = validObj;
-				next(err);
-			}, [key], options);
-		};
-
-		// Formalize and store finalSchema if not present. Otherwise just validate.
-		if (finalSchema === undefined) {
-      isvalid.formalize(schema, function(formalizedSchema) {
-			   finalSchema = formalizedSchema;
-			   validate();
-		  });
-    } else {
-			validate();
-		}
-
+		isvalid(req.body, formalizedSchema, function(err, validData) {
+			req.body = validData;
+			next(err);
+		}, ['body'], options);
 	};
 
 };
 
-exports.body = module.exports.body = function(schema, options) {
-	return middleware('body', schema, options);
-};
-
 exports.query = module.exports.query = function(schema, options) {
-	return middleware('query', schema, options);
+
+	var formalizedSchema = formalize(schema);
+
+	return function(req, res, next) {
+		isvalid(req.query, formalizedSchema, function(err, validData) {
+			req.query = validData;
+			next(err);
+		}, ['query'], options);
+	};
+
 };
 
 exports.param = module.exports.param = function(schema, options) {
 
-	var finalSchema;
+	var formalizedSchema = formalize(schema);
 
 	return function(req, res, next, val, id) {
-
-		function validate() {
-			isvalid(req.params[id], finalSchema, function(err, validData) {
-				req.params[id] = validData;
-				next(err);
-			}, [id], options);
-		};
-
-		if (finalSchema === undefined) {
-			isvalid.formalize(schema, function(formalizedSchema) {
-				finalSchema = formalizedSchema;
-				validate();
-			});
-		} else {
-			validate();
-		}
-
+		isvalid(req.params[id], formalizedSchema, function(err, validData) {
+			req.params[id] = validData;
+			next(err);
+		}, [id], options);
 	};
+
 };
--- packages/isvalid/test/middleware.js
+++ packages/isvalid/test/middleware.js
@@ -29,7 +29,7 @@ app.get('/query',
 
 app.post('/post',
   validate.body({
-    'test': { type: String, match: /^.*?test$/i }
+    'test': [ { type: String, match: /^.*?test$/i } ]
   }),
   function(req, res) {
     res.status(200).json(req.body);
@@ -49,59 +49,72 @@ app.use(function(err, req, res, next) {
   res.status(500).json({error: 'internal-server-error'});
 });
 
-describe('middlewares', function() {
+describe('middleware', function() {
 
-  it ('should come back with 400 if param does not match schema', function(done) {
-    request(app)
-    .get('/param/test')
-    .expect(400, function(err, res) {
-      expect(res.body).to.have.property('error').equal('validation-error');
-      done(err);
+  describe('param validator', function() {
+
+    it ('should come back with 400 if param does not match schema', function(done) {
+      request(app)
+      .get('/param/test')
+      .expect(400, function(err, res) {
+        expect(res.body).to.have.property('error').equal('validation-error');
+        done(err);
+      });
     });
+
+    it ('should come back with 200 if param matches schema', function(done) {
+      request(app)
+      .get('/param/123')
+      .expect(200, done);
+    });
+
   });
 
-  it ('should come back with 200 if param matches schema', function(done) {
-    request(app)
-    .get('/param/123')
-    .expect(200, done);
+  describe('query validator', function() {
+
+    it ('should come back with 400 if query matches schema', function(done) {
+      request(app)
+      .get('/query?test=nonmatching')
+      .expect(400, function(err, req) {
+        expect(req.body).to.have.property('error').equal('validation-error');
+        done(err);
+      });
+    });
+
+    it ('should come back with 200 and correct body if query matches schema', function(done) {
+      request(app)
+      .get('/query?test=myTest')
+      .expect(200, function(err, res) {
+        expect(res.body).to.have.property('test').equal('myTest');
+        done(err);
+      });
+    });
+
   });
 
-  it ('should come back with 400 if query matches schema', function(done) {
-    request(app)
-    .get('/query?test=nonmatching')
-    .expect(400, function(err, req) {
-      expect(req.body).to.have.property('error').equal('validation-error');
-      done(err);
-    });
-  });
+  describe('body validator', function() {
 
-  it ('should come back with 200 and correct body if query matches schema', function(done) {
-    request(app)
-    .get('/query?test=myTest')
-    .expect(200, function(err, res) {
-      expect(res.body).to.have.property('test').equal('myTest');
-      done(err);
+    it ('should come back with 400 if post body does not match schema', function(done) {
+      request(app)
+      .post('/post')
+      .send({ test: 'nonmatching' })
+      .expect(400, function(err, res) {
+        expect(res.body).to.have.property('error').equal('validation-error');
+        done(err);
+      });
     });
-  });
 
-  it ('should come back with 400 if post body does not match schema', function(done) {
-    request(app)
-    .post('/post')
-    .send({ test: 'nonmatching' })
-    .expect(400, function(err, res) {
-      expect(res.body).to.have.property('error').equal('validation-error');
-      done(err);
+    it ('should come back with 200 and correct body set if post body matches schema', function(done) {
+      request(app)
+      .post('/post')
+      .send({ test: ['myTest'] })
+      .expect(200, function(err, res) {
+        expect(res.body).to.have.property('test').to.be.an('Array').of.length(1);
+        expect(res.body.test[0]).to.be.a('String').equal('myTest');
+        done(err);
+      });
     });
-  });
 
-  it ('should come back with 200 and correct body set if post body matches schema', function(done) {
-    request(app)
-    .post('/post')
-    .send({ test: 'myTest' })
-    .expect(200, function(err, res) {
-      expect(res.body).to.have.property('test').equal('myTest');
-      done(err);
-    });
   });
 
 });
