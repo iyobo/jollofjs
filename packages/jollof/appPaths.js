4172974d35593000634ca1b0141c0fb4aefacfc2
iyobo Eki <phil.eki@gmail.com>
2016-10-27 16:36:29 -0400

    Beginnings of new schema system

--- packages/jollof/appPaths.js
+++ packages/jollof/appPaths.js
@@ -2,12 +2,14 @@
  * Created by iyobo on 2016-10-17.
  */
 const path = require('path');
+const root = process.cwd();
 
 module.exports={
-	appRoot: process.cwd() + '/',
-	models: path.join('..','..','app','mongomodels'),
-	services: "../../app/services",
-	viewFilters: '../../app/views/filters',
-	config: '../../config',
-	commands: '../../commands'
+	appRoot: root,
+	models: path.join(root,'app','models'),
+	schemaTypes: path.join(root,'app','types'),
+	services: path.join(root,'app','services'),
+	viewFilters: path.join(root,'app','views','filters'),
+	config: path.join(root,'config'),
+	commands: path.join(root,'app','commands')
 }
