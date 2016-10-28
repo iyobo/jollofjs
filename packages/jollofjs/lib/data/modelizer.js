9025705b35332db7b529c96d3f463e134698d2de
iyobo Eki <phil.eki@gmail.com>
2016-10-27 22:56:06 -0400

    modelizer checkpoint

--- packages/jollofjs/lib/data/modelizer.js
+++ packages/jollofjs/lib/data/modelizer.js
@@ -1,7 +1,8 @@
 /**
  * Created by iyobo on 2016-10-27.
  */
-
+const valix = require('validate');
+const _ = require('lodash');
 
 /**
  * Takes a schema. Returns a model
@@ -10,45 +11,87 @@
  */
 module.exports.modelize = function ( schema ) {
 
+	const modelRules={};
+
+	//For each field defined in structure
+	for(let fieldName in schema.structure){
+		const rules={}
+		const attrs = schema.structure[fieldName];
+
+		//craft validation rules from attrs
+		let gg = typeof attrs;
+		//
+
+		const coreType = typeof attrs;
+		let fieldType = "";
+		if(coreType === 'function'){
+			//This means it's a class e.g. Boolean, String, etc.
+			fieldType = attrs.name;
+		}else{
+			//This means it is a full schema field def, and so must have type field
+			fieldType = attrs.type.name;
+		}
+
+		console.log(fieldName, fieldType);
+
+		//Add crafted field constraints to model validation rules
+		modelRules[fieldName] = rules;
+	}
+
+	/**
+	 * This is a dynamically crafted class with tons of accessor magic. E.g userInstance.firstName will return userInstance.data.firstName
+	 * @type {{updateAll: ((id)), update: ((id)), list: ((id)), get: ((id)), delete: ((id)), new(data)=>{validate: (()), save: (()), type}}}
+	 */
 	const Model = class {
-		constructor(data){
+		constructor( data ) {
 			this.data = data;
+
+			this.rules = modelRules;
 		}
 
-		static * get(id){
-			return 'Got '+schema.name;
+		get type() {
+			return schema.name;
 		}
 
-		static * list(id){
-			return 'List '+schema.name;
+		static * get( id ) {
+			return 'Got ' + schema.name;
 		}
 
-		static * update(id){
+		static * list( id ) {
+			return 'List ' + schema.name;
+		}
+
+		static * update( id ) {
 			return "Hohoho";
 		}
 
-		static * updateAll(id){
+		static * updateAll( id ) {
 			return "Hohoho";
 		}
 
-		static * delete(id){
+		static * delete( id ) {
 			return "Hohoho";
 		}
 
-		validate(){
+		validate() {
+			// validate it according to structure
+
+
 			return true;
 		}
 
-		* save (){
-			if(this.validate()){
-				return "Saving "+schema.name;
+		* save() {
+			if (this.validate()) {
+				return this;
+			}
+			else {
+				return null;
 			}
 		}
 
 
-
 	};
 
-	// This way when we can new jollof.models.user({firstName:'Joe'})
+	// This way we can new jollof.models.user({firstName:'Joe'})
 	return Model;
 }
