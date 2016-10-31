50c5b4b5c6074b44bfbd2cf0b6c86d7040cdccb3
iyobo Eki <phil.eki@gmail.com>
2016-10-31 19:03:19 -0400

    Add support for Persistent Index

--- packages/jollof/lib/data/modelizer.js
+++ packages/jollof/lib/data/modelizer.js
@@ -7,12 +7,26 @@ const Promise = require('bluebird')
 const joiValidatePromise = Promise.promisify(joi.validate);
 const stringUtil = require('../util/stringUtil');
 const config = require('../configurator');
+
 /**
- * Takes a schema. Wraps it in a joi object, and Returns a model
+ * Here we use the adapter to setup the collection this Model represents
+ * @param adapter
+ * @param schema
+ */
+
+
+/**
+ * Takes a schema. Returns a Model
  * @param schema
  * @returns {*}
  */
-module.exports.modelize = function ( schema ) {
+module.exports.modelize = function( schema ) {
+	const adapterName = this._adapterName = schema.adapter || config.settings.db.defaultAdapter || 'arangodb'; //:) arango is default just because
+	const adapter = require('jollof-data-' + this._adapterName)(config.settings.db[ adapterName ]);
+
+	//create tables, indexes, etc
+
+	adapter.configureCollection(schema);
 
 	/**
 	 * This is a dynamically crafted class with some accessor/proxy magic. E.g userInstance.firstName will return userInstance.data.firstName
@@ -22,7 +36,8 @@ module.exports.modelize = function ( schema ) {
 		constructor( data ) {
 			this._loadData(data);
 			this._loadRules(schema.structure);
-			this._adapter = schema.adapter || config.settings.db.defaultAdapter || 'arangodb'; //:) arango is default just because
+			this._adapterName = adapterName;
+			// this._adapter = adapter;
 
 			this._className = stringUtil.labelize(schema.name);
 			this._originalSchema = schema;
@@ -51,6 +66,7 @@ module.exports.modelize = function ( schema ) {
 		}
 
 		static * get( id ) {
+
 			return 'Got ' + schema.name;
 		}
 
@@ -94,19 +110,19 @@ module.exports.modelize = function ( schema ) {
 	const ModelProxy = class {
 		constructor( data ) {
 			let dataKeys = Object.keys(schema.structure);
-			dataKeys.push('dateCreated','lastUpdated');
+			dataKeys.push('dateCreated', 'lastUpdated');
 
 			var modelAccessor = {
 				set ( target, key, value ) {
-					target._data[key] = value;
+					target._data[ key ] = value;
 					return true
 				},
 				get ( target, key ) {
 					//if key exists in data's keys, get from there. otherwise get from the model
-					if(dataKeys.indexOf(key)>-1)
-						return target._data[key];
+					if (dataKeys.indexOf(key) > -1)
+						return target._data[ key ];
 					else
-						return target[key];
+						return target[ key ];
 				}
 			}
 
