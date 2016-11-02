d8089aa96e2c3d1ae89f32344dfca205b4ede1b9
iyobo Eki <phil.eki@gmail.com>
2016-11-02 04:59:21 -0400

    Implemented Model Id storage

--- packages/jollofjs/lib/data/modelizer.js
+++ packages/jollofjs/lib/data/modelizer.js
@@ -7,6 +7,7 @@ const Promise = require('bluebird')
 const joiValidatePromise = Promise.promisify(joi.validate);
 const stringUtil = require('../util/stringUtil');
 const config = require('../configurator');
+const log = require('../log');
 
 /**
  * Here we use the adapter to setup the collection this Model represents
@@ -20,7 +21,7 @@ const config = require('../configurator');
  * @param schema
  * @returns {*}
  */
-module.exports.modelize = function( schema ) {
+module.exports.modelize = function ( schema ) {
 	const adapterName = this._adapterName = schema.adapter || config.settings.db.defaultAdapter || 'arangodb'; //:) arango is default just because
 	const adapter = require('jollof-data-' + this._adapterName)(config.settings.db[ adapterName ]);
 
@@ -39,8 +40,12 @@ module.exports.modelize = function( schema ) {
 			this._adapterName = adapterName;
 			// this._adapter = adapter;
 
-			this._className = stringUtil.labelize(schema.name);
+			this._collectionName = schema.name;
+			this._collectioonDisplayName = stringUtil.labelize(schema.name);
 			this._originalSchema = schema;
+
+			//This is where ids should be stored
+			this._ids = null;
 		}
 
 		_loadData( data ) {
@@ -48,7 +53,20 @@ module.exports.modelize = function( schema ) {
 		}
 
 		_loadRules( structure ) {
-			this._rules = joi.object().keys(structure);
+
+			const tempStructure = _.assign({}, structure);
+
+			//Add DB adapter meta fields
+			_.each(adapter.getIdFields(), ( fieldName )=> {
+				tempStructure[ fieldName ] = joi.any().optional();
+			});
+
+			//Add date fields
+			tempStructure[ 'dateCreated' ] = joi.date().optional();
+			tempStructure[ 'lastUpdated' ] = joi.date().optional();
+
+			//Add schema rules
+			this._rules = joi.object().keys(tempStructure);
 		}
 
 		_setDateCreated() {
@@ -70,15 +88,11 @@ module.exports.modelize = function( schema ) {
 			return 'Got ' + schema.name;
 		}
 
-		static * list( id ) {
+		static * find( id ) {
 			return 'List ' + schema.name;
 		}
 
 		static * update( id ) {
-			return "Hohoho";
-		}
-
-		static * updateAll( id ) {
 			return "Hohoho";
 		}
 
@@ -87,7 +101,6 @@ module.exports.modelize = function( schema ) {
 		}
 
 		* validate() {
-
 			yield joiValidatePromise(this._data, this._rules);
 		}
 
@@ -96,8 +109,10 @@ module.exports.modelize = function( schema ) {
 				//validate
 				yield this.validate();
 
-				//TODO: yield save action with active adapter
 				this._setTimestamps();
+				yield adapter.save(this);
+				// log.debug('Saved ' + this._collectionName)
+				null;
 
 			} catch (err) {
 				throw err;
@@ -112,21 +127,30 @@ module.exports.modelize = function( schema ) {
 			let dataKeys = Object.keys(schema.structure);
 			dataKeys.push('dateCreated', 'lastUpdated');
 
+			const model = new Model(data);
+
 			var modelAccessor = {
 				set ( target, key, value ) {
-					target._data[ key ] = value;
+					if (dataKeys.indexOf(key) > -1)
+						target._data[ key ] = value;
+					else if (adapter.getIdFields().indexOf(key) > -1)
+						target._ids[ key ] = value;
+					else
+						target[ key ] = value;
 					return true
 				},
 				get ( target, key ) {
 					//if key exists in data's keys, get from there. otherwise get from the model
 					if (dataKeys.indexOf(key) > -1)
 						return target._data[ key ];
+					else if (adapter.getIdFields().indexOf(key) > -1)
+						return target._ids[ key ];
 					else
 						return target[ key ];
 				}
 			}
 
-			return new Proxy(new Model(data), modelAccessor);
+			return new Proxy(model, modelAccessor);
 		}
 	}
 
