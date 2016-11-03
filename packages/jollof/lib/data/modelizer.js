3091818ce59e4cd1c7c2710633d6a9f435a5c3f9
iyobo Eki <phil.eki@gmail.com>
2016-11-03 18:19:40 -0400

    Models: Can now get by Id, and minimum find by criteria

--- packages/jollof/lib/data/modelizer.js
+++ packages/jollof/lib/data/modelizer.js
@@ -24,7 +24,7 @@ const log = require('../log');
 module.exports.modelize = function ( schema ) {
 	const adapterName = this._adapterName = schema.adapter || config.settings.db.defaultAdapter || 'arangodb'; //:) arango is default just because
 	const adapter = require('jollof-data-' + this._adapterName)(config.settings.db[ adapterName ]);
-
+	const collectionName = schema.name;
 	//create tables, indexes, etc
 
 	// adapter.configureCollection(schema);
@@ -44,8 +44,7 @@ module.exports.modelize = function ( schema ) {
 			this._collectioonDisplayName = stringUtil.labelize(schema.name);
 			this._originalSchema = schema;
 
-			//This is where ids should be stored
-			this._ids = null;
+			this._idField = "_id"; //by default
 
 
 			//Proxy magic
@@ -53,28 +52,27 @@ module.exports.modelize = function ( schema ) {
 			dataKeys.push('dateCreated', 'lastUpdated');
 
 			// const model = new Model(data);
-
 			var modelAccessor = {
 				set ( target, key, value ) {
-					if(target._isDeleted)
+					if (target._isDeleted)
 						throw new Error('Jollof Data: Cannot set data for a deleted item');
 
 					if (dataKeys.indexOf(key) > -1)
 						target._data[ key ] = value;
-					else if (adapter.getIdFields().indexOf(key) > -1)
+					else if (adapter.idFields.indexOf(key) > -1)
 						target._ids[ key ] = value;
 					else
 						target[ key ] = value;
 					return true
 				},
 				get ( target, key ) {
-					if(target._isDeleted)
-						throw new Error('Jollof Data: Cannot set data for a deleted item');
+					if (target._isDeleted)
+						throw new Error('Jollof Data: Cannot get data from a deleted item');
 
 					//if key exists in data's keys, get from there. otherwise get from the model
 					if (dataKeys.indexOf(key) > -1)
 						return target._data[ key ];
-					else if (adapter.getIdFields().indexOf(key) > -1)
+					else if (adapter.idFields.indexOf(key) > -1)
 						return target._ids[ key ];
 					else
 						return target[ key ];
@@ -84,6 +82,14 @@ module.exports.modelize = function ( schema ) {
 			return new Proxy(this, modelAccessor);
 		}
 
+		isPersisted(){
+			return this._ids && this._ids.length > 0;
+		}
+
+		/**
+		 * Must be called once, during boot, to configure the database with
+		 * collections/indices for this Model.
+		 */
 		static * setup() {
 			const g = yield adapter.configureCollection(schema);
 			console.log(g);
@@ -93,8 +99,123 @@ module.exports.modelize = function ( schema ) {
 			return schema.name;
 		}
 
+		static get adapterName() {
+			return adapterName;
+		}
+
+		static get adapter() {
+			return adapter;
+		}
+
+		static * get( id, params ) {
+			const val= yield adapter.get(collectionName, id, params || {});
+			return Model.instantiate(val);
+		}
+
+		/**
+		 * Digests an array or object and returns either an array or singular model instance of the model.
+		 * @param data
+		 * @returns {*}
+		 */
+		static instantiate(data) {
+
+			if (!data)
+				return null;
+
+			if (Array.isArray(data)) {
+				//This is an array of items
+				return data.map((item)=>{
+					return new Model(item);
+				});
+			}
+			else {
+				//a singular item. wrap it
+				return new Model(data);
+			}
+		}
+
+		/**
+		 *
+		 * @param criteria
+		 * @param params
+		 * @returns {*}
+		 */
+		static * find( criteria, params ) {
+			const res= yield adapter.find(collectionName, criteria, params || {});
+			return Model.instantiate(res);
+		}
+
+		/**
+		 *
+		 * @param criteria
+		 * @param params
+		 * @returns {*}
+		 */
+		static * findOne( criteria, params ) {
+			params = params || {};
+			params.limit = 1;
+			const res= yield adapter.find(collectionName, criteria, params);
+			return Model.instantiate(res);
+		}
+
+		/**
+		 *
+		 * @param criteria
+		 * @param params
+		 * @returns {*}
+		 */
+		static * update( criteria, params ) {
+			return yield adapter.update(collectionName, criteria, params || {});
+		}
+
+		/**
+		 *
+		 * @param criteria
+		 * @param params
+		 * @returns {*}
+		 */
+		static * updateOne( criteria, params ) {
+			params = params || {};
+			params.limit = 1;
+			return yield adapter.update(collectionName, criteria, params);
+		}
+
+		/**
+		 *
+		 * @param criteria
+		 * @param params
+		 * @returns {*}
+		 */
+		static * remove( criteria, params ) {
+			return yield adapter.remove(collectionName, criteria, params || {});
+		}
+
+		/**
+		 *
+		 * @param criteria
+		 * @param params
+		 * @returns {*}
+		 */
+		static * removeOne( criteria, params ) {
+			params = params || {};
+			params.limit = 1;
+			return yield adapter.remove(collectionName, criteria, params);
+		}
+
 		_loadData( data ) {
-			this._data = data;
+
+			this._ids = {};
+			this._data = {};
+
+			//seperate data from ids
+			for (let key in data) {
+				if (adapter.idFields.indexOf(key) > -1) {
+					this._ids[ key ] = data[ key ];
+				}
+				else {
+					this._data[ key ] = data[ key ];
+				}
+			}
 		}
 
 		_loadRules( structure ) {
@@ -102,7 +223,7 @@ module.exports.modelize = function ( schema ) {
 			const tempStructure = _.assign({}, structure);
 
 			//Add DB adapter meta fields
-			_.each(adapter.getIdFields(), ( fieldName )=> {
+			_.each(adapter.idFields, ( fieldName )=> {
 				tempStructure[ fieldName ] = joi.any().optional();
 			});
 
@@ -128,37 +249,53 @@ module.exports.modelize = function ( schema ) {
 			this._setDateUpdated();
 		}
 
-		static get collectionName() {
-			return schema.name;
-		}
 
 		* validate() {
 			yield joiValidatePromise(this._data, this._rules);
 		}
 
+		/**
+		 * Persists the model object in the DB
+		 * @returns {boolean}
+		 */
 		* save() {
 
 			//validate
 			yield this.validate();
 
 			this._setTimestamps();
-			yield adapter.save(this);
-			// log.debug('Saved ' + this._collectionName)
-			return true;
-
+			const res = yield adapter.save(this);
+			log.debug('Saved ' + this._ids[ adapter.idField ]);
+			return res;
 		}
 
-		_destroy(){
-			this._isDeleted = true;
-			// this._data = null;
+		/**
+		 * Marks this item as deleted.
+		 * @private
+		 */
+		_markDeleted() {
+
+			this._data = null;
 			this._ids = null;
+			this._isDeleted = true;
 		}
 
-		* delete(){
-			yield adapter.delete(this);
+		/**
+		 * Delete this item from the database
+		 * @returns {boolean}
+		 */
+		* delete() {
+			yield adapter.remove(this);
 
-			this._destroy();
+			this._markDeleted();
 			return true;
+		}
+
+		/**
+		 * The json representation of this model's combined data.
+		 */
+		display() {
+			return _.assign(this._ids, this._data);
 		}
 
 	};
