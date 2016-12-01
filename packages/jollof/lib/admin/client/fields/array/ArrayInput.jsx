6e88efb45f8fa629a031ac6cd2ba36337eba7cd8
iyobo Eki <phil.eki@gmail.com>
2016-12-01 12:17:23 -0500

    array input in-progress

--- packages/jollof/lib/admin/client/fields/array/ArrayInput.jsx
+++ packages/jollof/lib/admin/client/fields/array/ArrayInput.jsx
@@ -1,10 +1,11 @@
 import React, {PropTypes, Component} from 'react';
 const uuid = require('uuid');
 const _ = require('lodash');
-import TextField from 'material-ui/TextField';
+import IconButton from 'material-ui/IconButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 /**
- * Renders multiple instances of given field
+ * handles array of given field
  */
 export class ArrayInput extends Component {
 	componentWillMount() {
@@ -20,11 +21,40 @@ export class ArrayInput extends Component {
 
 	}
 
+	onAddItem( evt ) {
+		const items = this.props.input.value === '' ? [] : this.props.input.value;
+		items.push('');
+		this.props.input.onChange(items);//???aa
+	}
+
+	onRemoveItem( evt , index) {
+		const items = this.props.input.value === '' ? [] : this.props.input.value;
+		items.splice(index,1);
+		this.props.input.onChange(items);
+	}
+
 
 	render() {
+		const Item = this.props.itemComponent;
+
+		// let items = [
+		// 	<Item key={uuid()} source={1} {...this.props.options}/>,
+		// 	<Item key={uuid()} source={2} {...this.props.options}/>,
+		// 	<Item key={uuid()} source={3} {...this.props.options}/>
+		// ]
+
+		let items = this.props.input.value.map ? this.props.input.value.map(( v )=> {
+			return <Item key={v} source={v} {...this.props.options}   />
+		}) : [];
+
 		return (
 			<div >
-
+				{items}
+				<div className="">
+					<IconButton onClick={this.onAddItem.bind(this)}>
+						<ContentAdd />
+					</IconButton>
+				</div>
 			</div>
 		);
 	}
@@ -32,8 +62,11 @@ export class ArrayInput extends Component {
 
 ArrayInput.propTypes = {
 	input: PropTypes.object,
-	field: PropTypes.object,
+	itemComponent: PropTypes.any,
 	label: PropTypes.string,
 	onChange: PropTypes.func,
-	source: PropTypes.string.isRequired
+	source: PropTypes.string.isRequired,
+	options: PropTypes.any,
+	meta: PropTypes.object,
+	name: PropTypes.string
 };
