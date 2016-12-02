01f7b4f26ffe1e46b823106a23686d1cf88c35ac
iyobo Eki <phil.eki@gmail.com>
2016-12-02 09:00:49 -0500

    ArrayInput can add and remove items

--- packages/jollofjs/lib/admin/client/fields/array/ArrayInput.jsx
+++ packages/jollofjs/lib/admin/client/fields/array/ArrayInput.jsx
@@ -3,6 +3,7 @@ const uuid = require('uuid');
 const _ = require('lodash');
 import IconButton from 'material-ui/IconButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentRemove from 'material-ui/svg-icons/content/remove';
 
 /**
  * handles array of given field
@@ -14,6 +15,7 @@ export class ArrayInput extends Component {
 
 		this.state = {
 			key: key,
+			items: []
 		}
 	}
 
@@ -21,36 +23,84 @@ export class ArrayInput extends Component {
 
 	}
 
-	onAddItem( evt ) {
-		const items = this.props.input.value === '' ? [] : this.props.input.value;
-		items.push('');
-		this.props.input.onChange(items);//???aa
+	_updateItems(items){
+		this.setState({items:items});
+		this.props.input.onChange(items);
+
+		console.log('this.state.items', this.state.items)
 	}
 
-	onRemoveItem( evt , index) {
-		const items = this.props.input.value === '' ? [] : this.props.input.value;
+	onAddItem( evt ) {
+		let items =  [ ...this.state.items, '' ];
+
+		this._updateItems(items);
+	}
+
+	onRemoveItem( evt, index ) {
+		let items =  [ ...this.state.items];
+
+		//remove index
 		items.splice(index,1);
-		this.props.input.onChange(items);
+
+		this._updateItems(items);
+	}
+
+	onChildChange( evt, i ) {
+		let items =  [ ...this.state.items]
+
+
+	}
+
+	onChildBlur( evt, i ) {
+
+	}
+
+	onDragStart( evt, i ) {
+
+	}
+
+	onDrop( evt, i ) {
+
+	}
+
+	onFocus( evt, i ) {
+
 	}
 
 
 	render() {
 		const Item = this.props.itemComponent;
+		let meta = {touched: <div></div>, error: null};
 
-		// let items = [
-		// 	<Item key={uuid()} source={1} {...this.props.options}/>,
-		// 	<Item key={uuid()} source={2} {...this.props.options}/>,
-		// 	<Item key={uuid()} source={3} {...this.props.options}/>
-		// ]
 
-		let items = this.props.input.value.map ? this.props.input.value.map(( v )=> {
-			return <Item key={v} source={v} {...this.props.options}   />
-		}) : [];
+		let items = this.state.items.map(( v, i )=> {
+
+			let options = {id: this.state.key + '_' + i}
+			let input = {
+				...this.props.input,
+				value: this.state.items[ i ],
+				onChange: this.onChildChange.bind(this, i),
+				onBlur: this.onChildBlur.bind(this, i),
+				onDragStart: this.onDragStart.bind(this, i),
+				onDrop: this.onDrop.bind(this, i),
+				onFocus: this.onFocus.bind(this, i),
+
+			}
+
+			return (
+				<div key={options.id}>
+					<IconButton onClick={this.onRemoveItem.bind(this,i)}>
+						<ContentRemove />
+					</IconButton>
+					<Item options={options} {...this.props} source={v} meta={meta} input={input}/>
+				</div>
+			)
+		});
 
 		return (
-			<div >
+			<div>
 				{items}
-				<div className="">
+				<div className="actions">
 					<IconButton onClick={this.onAddItem.bind(this)}>
 						<ContentAdd />
 					</IconButton>
@@ -68,5 +118,9 @@ ArrayInput.propTypes = {
 	source: PropTypes.string.isRequired,
 	options: PropTypes.any,
 	meta: PropTypes.object,
-	name: PropTypes.string
+	name: PropTypes.string,
+
+	style: PropTypes.object,
+	type: PropTypes.string,
+	validation: PropTypes.object,
 };
