52cd611a44d6173d026cada01f8fe2948ed1cf5f
iyobo Eki <phil.eki@gmail.com>
2017-05-07 20:41:51 -0400

    init

---
+++ packages/jollof-scaffold/config/base.js
@@ -0,0 +1,23 @@
+/**
+ * Created by iyobo on 2016-11-07.
+ */
+
+module.exports = {
+
+	env: 'development',
+
+    // Uncomment to use mongodb as Default datasource. Otherwise an in-Memory datasource will be used.
+    //data: {
+    //    dataSources:{
+    //        default: {
+    //            adapter: require('jollof-data-mongodb'),
+    //            nativeType: 'mongodb',
+    //            options: {
+    //                mongoUrl: 'mongodb://localhost/jollofdb'
+    //            }
+    //        }
+    //    }
+    //},
+
+}
+
---
+++ packages/jollof-scaffold/index.js
@@ -0,0 +1,6 @@
+const jollof = require('jollof');
+
+
+jollof.bootstrap.bootServer(function*(app){
+    jollof.log.info('Server started');
+});
