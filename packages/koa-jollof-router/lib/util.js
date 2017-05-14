dfec333d1fd556abdc2ef2739bfb7b4e29ae812c
iyobo Eki <phil.eki@gmail.com>
2017-05-14 19:05:11 -0400

    Implemented

--- packages/koa-jollof-router/lib/util.js
+++ packages/koa-jollof-router/lib/util.js
@@ -2,13 +2,64 @@
  * Created by iyobo on 2017-05-14.
  */
 
-exports.digestRouteMap = (router, routes)=>{
+const KRouter = require('koa-router');
+
+function isPath(it) {
+
+    return it.startsWith('/')
+}
+
+function makeRoute(router, method, path, flow, children) {
+
+    if(!children){
+        router = router[method](path, flow )
+    }else{
+        //has children
+        let subRouter = KRouter();
+        subRouter = digestRouteMap(subRouter, children)
+
+        router.use(path, subRouter.routes(), subRouter.allowedMethods());
+    }
+
+
+    return router;
+}
+
+exports.digestRouteMap = (router, routes) => {
 
     const keys = Object.keys(routes);
 
-    for (const key in keys){
+    for (let key in keys) {
+
         const value = routes[key];
 
+        //flow and children are the currently supported route fields
+        const flow = value.flow;
+        const children = value.children;
+
+        //key contains method and path
+        key = key.trim();
+        let methPath = key.split(' ');
+        let method = 'get';
+        let path;
+
+        methPath.forEach((it) => {
+            it = it.trim();
+            if (isPath(it)) {
+                path = it;
+            } else {
+                method = it.toLowerCase();
+            }
+        });
+
+        //At this point we should have a path!
+        if (!path) {
+            throw new Error('No path defined for route:', key)
+        }
+
+        router = makeRoute(router, method, path, flow, children);
 
     }
+
+    return router;
 }
