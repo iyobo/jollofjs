c3d682a066eb7eda747680f4de0f2c01229e40c2
iyobo Eki <phil.eki@gmail.com>
2017-05-23 00:02:48 -0400

    Setting MongoDb to be the default DB

--- packages/jollof-cli/jollof-scaffold/index.js
+++
@@ -1,9 +0,0 @@
-const figlet = require('figlet')
-console.log(figlet.textSync('JollofJS'));
-
-const jollof = require('jollof');
-
-
-jollof.bootstrap.bootServer(function*(app) {
-    jollof.log.info('Server started');
-});
