ba81ce481e4b6ce109b3fc73e6f4e4db9189e542
iyobo Eki <phil.eki@gmail.com>
2017-05-28 13:26:46 -0500

    Renaming!

--- packages/jollof-app/copyToCli.sh
+++ packages/jollof-app/copyToCli.sh
@@ -1,7 +1,7 @@
-cp -r ./app ../jollof-cli/jollof-scaffold/
-cp -r ./commands ../jollof-cli/jollof-scaffold/
-cp -r ./config ../jollof-cli/jollof-scaffold/
-cp -r ./static ../jollof-cli/jollof-scaffold/
-cp  ./.gitignore ../jollof-cli/jollof-scaffold/.gitignore
-cp  ./index.js ../jollof-cli/jollof-scaffold/index.js
-cp  ./package.json ../jollof-cli/jollof-scaffold/package.json
+cp -r ./app ../jollof-cli/jollof-app/
+cp -r ./commands ../jollof-cli/jollof-app/
+cp -r ./config ../jollof-cli/jollof-app/
+cp -r ./static ../jollof-cli/jollof-app/
+cp  ./.gitignore ../jollof-cli/jollof-app/.gitignore
+cp  ./index.js ../jollof-cli/jollof-app/index.js
+cp  ./package.json ../jollof-cli/jollof-app/package.json
