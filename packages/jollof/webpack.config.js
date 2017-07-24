24ab84686ad1c45019a4bc6f39704cfc240d8829
iyobo Eki <phil.eki@gmail.com>
2017-07-24 00:47:17 -0400

    check

--- packages/jollof/webpack.config.js
+++ packages/jollof/webpack.config.js
@@ -75,20 +75,7 @@ module.exports = {
 			//		}
 			//	}
 			//},
-            {
-                test: require.resolve('tinymce/tinymce'),
-                loaders: [
-                    'imports?this=>window',
-                    'exports?window.tinymce'
-                ]
-            },
-            {
-                test: /tinymce\/(themes|plugins)\//,
-                loaders: [
-                    'imports?this=>window'
-                ]
-            }
-        ]
+		]
 	},
 	resolve: {
 		extensions: ['', '.js', '.jsx', '.vue'],
