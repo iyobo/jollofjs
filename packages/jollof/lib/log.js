9b5de65ae0810f608c8f96ec3fbc4570206f876c
iyobo Eki <phil.eki@gmail.com>
2017-07-25 23:09:04 -0400

    Better logs

--- packages/jollof/lib/log.js
+++ packages/jollof/lib/log.js
@@ -1,14 +1,27 @@
 'use strict'
+const Tracer = require('tracer');
+
 let log;
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development')
-    log = require('tracer').colorConsole({
-        format: [
-            "{{timestamp}} [{{file}}:{{line}}] {{message}} ",
-            { error: "{{timestamp}} [{{file}}:{{line}} - ERROR] {{message}} " }
-        ],
-    });
-else
-    log = require('tracer').dailyfile({ root: '../logs/', maxLogFiles: 10 });
+
+log = Tracer.colorConsole({
+    format: [
+        "{{timestamp}} [{{file}}:{{line}}] {{message}} ",
+        { error: "{{timestamp}} [{{file}}:{{line}} - ERROR] {{message}} " }
+    ],
+    transport: {
+        function (data) {
+            fs.appendFile('./logs/file.log', data.output + '\n', (err) => {
+                if (err) throw err;
+            });
+        },
+        function (data) {
+            console.log(data.output);
+        }
+    }
+});
+//if (process.env.NODE_ENV !== 'development')
+//    log = Tracer.dailyfile({ root: './logs/', maxLogFiles: 10 });
+
 
 log.err = log.error;
 
