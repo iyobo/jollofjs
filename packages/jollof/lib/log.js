f98be06421335f4b042f33310dd27ae80d3fe684
iyobo Eki <phil.eki@gmail.com>
2017-07-26 00:27:32 -0400

    Better logs

--- packages/jollof/lib/log.js
+++ packages/jollof/lib/log.js
@@ -8,16 +8,6 @@ log = Tracer.colorConsole({
         "{{timestamp}} [{{file}}:{{line}}] {{message}} ",
         { error: "{{timestamp}} [{{file}}:{{line}} - ERROR] {{message}} " }
     ],
-    transport: {
-        function (data) {
-            fs.appendFile('./logs/file.log', data.output + '\n', (err) => {
-                if (err) throw err;
-            });
-        },
-        function (data) {
-            console.log(data.output);
-        }
-    }
 });
 //if (process.env.NODE_ENV !== 'development')
 //    log = Tracer.dailyfile({ root: './logs/', maxLogFiles: 10 });
