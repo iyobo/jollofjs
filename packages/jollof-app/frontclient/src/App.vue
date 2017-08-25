168d67981c183608ace54dded91d3024235bb80b
iyobo Eki <phil.eki@gmail.com>
2017-08-25 11:53:51 -0400

    Publish
     - jollof-app@1.1.18
     - jollof-spice-blog@0.1.17
     - jollof@1.0.23

---
+++ packages/jollof-app/frontclient/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="{'nav-open': $sidebar.showSidebar}">
+    <router-view></router-view>
+    <!--This sidebar appears only for screens smaller than 992px-->
+    <side-bar type="navbar" :sidebar-links="$sidebar.sidebarLinks">
+      <ul class="nav navbar-nav">
+        <li>
+          <a class="dropdown-toggle" data-toggle="dropdown">
+            <i class="ti-panel"></i>
+            <p>Stats</p>
+          </a>
+        </li>
+        <drop-down title="5 Notifications" icon="ti-bell">
+
+          <li><a>Notification 1</a></li>
+          <li><a>Notification 2</a></li>
+          <li><a>Notification 3</a></li>
+          <li><a>Notification 4</a></li>
+          <li><a>Another notification</a></li>
+
+        </drop-down>
+        <li>
+          <a>
+            <i class="ti-settings"></i>
+            <p>Settings</p>
+          </a>
+        </li>
+        <li class="divider"></li>
+      </ul>
+    </side-bar>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss"></style>
---
+++ packages/jollof-app/frontclient/src/main.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import vClickOutside from 'v-click-outside'
+
+// Plugins
+import GlobalComponents from './gloablComponents'
+import Notifications from './components/UIComponents/NotificationPlugin'
+import SideBar from './components/UIComponents/SidebarPlugin'
+import App from './App'
+
+// router setup
+import routes from './routes/routes'
+
+// library imports
+import Chartist from 'chartist'
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/sass/paper-dashboard.scss'
+import 'es6-promise/auto'
+
+// plugin setup
+Vue.use(VueRouter)
+Vue.use(GlobalComponents)
+Vue.use(vClickOutside)
+Vue.use(Notifications)
+Vue.use(SideBar)
+
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkActiveClass: 'active'
+})
+
+// global library setup
+Object.defineProperty(Vue.prototype, '$Chartist', {
+  get () {
+    return this.$root.Chartist
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+  data: {
+    Chartist: Chartist
+  }
+})
